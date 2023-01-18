c5c498325f2fb7fd6a24cb2261b3d063003d6a6a
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-18 12:09:25 -0300

    feat: def scrape_news responsável por buscar as informações das notícias para preencher um dicionário

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,43 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css(
+        "link[href^='https://blog.betrybe.com']::attr(href)"
+    ).get()
+
+    title = selector.css("h1.entry-title::text").get()
+
+    timestamp = selector.css(".meta-date::text").get()
+
+    writer = selector.css(".author a::text").get()
+
+    comments_count = selector.css(".title-block::text").re_first(r"\d")
+    if comments_count is None:
+        comments_count = 0
+
+    unedited_summary = selector.css(
+        ".entry-content > p:nth-child(1) *::text"
+    ).getall()
+
+    summary = "".join(unedited_summary)
+
+    tags = selector.css(".post-tags a::text").getall()
+
+    category = selector.css(".category-style .label::text").get()
+
+    notice_data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "tags": tags,
+        "category": str(category),
+    }
+
+    return notice_data
 
 
 # Requisito 5
