b22d7ec98e1456f45453c967abf491c7ea5b2caf
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-18 20:57:43 -0300

    feat: def search_by_title realiza busca no DB por title informado e retorna uma lista de tuplas contendo o valor de title e url da notícia

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_found = search_news({"title": {"$regex": title.lower()}})
+    # resumed_news_info = [(news["title"], news["url"]) for news in news_found]
+    resumed_news_info = []
+    for news in news_found:
+        resumed_news_info.append((news["title"], news["url"]))
+    return resumed_news_info
 
 
 # Requisito 7
