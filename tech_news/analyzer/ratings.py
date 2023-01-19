0006cf917402da7121f2cbcb106b9fc8cc4353bd
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2023-01-19 19:13:19 -0300

    feat: def top_5_news realiza busca no DB pela quantidde comentários em ordem descrecente  e retorna uma lista de tuplas contendo o valor de title e url da notícia

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+from tech_news.analyzer.search_engine import resumed_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_found = search_news(
+        {"$query": {}, "$orderby": {"comments_count": -1, "title": 1}}
+    )
+    filtered_news = news_found[:5]
+    return resumed_news(filtered_news)
 
 
 # Requisito 11
